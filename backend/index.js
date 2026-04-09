bdfefc602d3ad5f960d6371cf863ef116d544234
Your Name <your.email@example.com>
2026-04-10 04:08:12 +0530

    Temporarily allow all origins for CORS

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "*", // Temporarily allow all origins
     credentials: true,
   }),
 );
